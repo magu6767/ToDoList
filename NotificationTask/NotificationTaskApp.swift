ba3b05396dff7206bb092f95fcb0c918285ea6dc
Hideto Maguchi <mogeko6347@gmail.com>
2023-02-06 20:16:56 +0900

    Initial Commit

---
+++ NotificationTask/NotificationTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationTaskApp.swift
+//  NotificationTask
+//
+//  Created by 間口秀人 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct NotificationTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
