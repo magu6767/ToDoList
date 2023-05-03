d50b6552193a91291b0d929abb13b0143217bc3e
Hideto Maguchi <hidetomaguchi@gmail.com>
2023-05-04 00:42:00 +0900

    1.Alert追加 2.初期化処理追加

--- NotificationTask/SetTimeView.swift
+++ NotificationTask/SetTimeView.swift
@@ -10,14 +10,20 @@ import SwiftUI
 struct SetTimeView: View {
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \TaskData.createdAt, ascending: false)]) private var taskData: FetchedResults<TaskData>
     private var notification =  Notification()
-    @State private var hour = 7
-    @State private var munite = 0
+    @State private var showingAlert = false
+    @State private var hour: Int
+    @State private var munite: Int
+    
+    init(hour: Int, munite: Int) {
+        _hour = State(initialValue: hour)
+        _munite = State(initialValue: munite)
+    }
     
     var body: some View {
         VStack {
             Text("通知する時間")
                 .padding()
-            Text(String(notification.hour) + " 時 " + String(notification.munite) + " 分")
+            Text(String(hour) + " 時 " + String(munite) + " 分")
             HStack {
                 Picker(selection: $hour, label: Text("")) {
                     ForEach(0..<24) { hour in
@@ -37,19 +43,18 @@ struct SetTimeView: View {
             }
             Button {
                 updateNotificationTime()
+                showingAlert = true
             } label: {
                 Text("決定")
                     .fontWeight(.bold)
                     .frame(width: 80)
             }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("通知時間を更新しました"),message: Text(""))   // 詳細メッセージの追加
+            }
             .buttonStyle(AnimationButtonStyle())
         }
         .navigationTitle("通知設定")
-        .onAppear{
-            //初期表示の設定
-            hour = notification.hour
-            munite = notification.munite
-        }
     }
     private func updateNotificationTime() {
         notification.hour = hour
@@ -71,11 +76,5 @@ struct AnimationButtonStyle : ButtonStyle {
             .compositingGroup()
             .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
             .animation(.linear, value: configuration.isPressed)
-    }
-}
-
-struct SetTimeView_Previews: PreviewProvider {
-    static var previews: some View {
-        SetTimeView()
     }
 }
